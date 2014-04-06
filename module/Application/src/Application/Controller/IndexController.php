8d22d241cb41dcbfe933482c9c19fc2f0c519dbd
ignasne <ignas.nedzinskas@gmail.com>
2014-04-06 17:17:07 +0300

    Simple SOAP service implementation

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -12,10 +12,56 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
+use Zend\Soap\Client;
+use Zend\Soap\Server;
+use Zend\Soap\AutoDiscover;
+
+require_once 'C:\www\zendskeleton\module\Application\src\Services\Services.php';
+
 class IndexController extends AbstractActionController
 {
+	public function init()
+	{
+
+	}
+
     public function indexAction()
     {
         return new ViewModel();
     }
+
+	public function soapAction()
+	{
+		$server = new Server(null,
+			array('uri' => 'http://zendskeleton.localhost/application/index/wsdl'));
+
+		$server->setClass('Services');
+
+		$server->handle();
+
+		return $this->getResponse();
+	}
+
+	public function wsdlAction()
+	{
+		$wsdl = new AutoDiscover();
+
+		$wsdl->setClass('Services');
+
+		$wsdl->setUri('http://zendskeleton.localhost/application/index/soap');
+
+		$wsdl->handle();
+		return $this->getResponse();
+	}
+
+	public function clientAction()
+	{
+		$url = 'http://zendskeleton.localhost/application/index/wsdl';
+
+		$client = new Client($url);
+
+		print_r($client->testSoap());
+
+		return $this->getResponse();
+	}
 }
---
+++ module/Application/src/Services/Services.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Services {
+
+	/**
+	 * Returns test list
+	 *
+	 * @return array
+	 */
+	public function testSoap()
+	{
+		$testResult = array(
+			'testResult'  => -10
+		);
+
+		return $testResult;
+	}
+}
